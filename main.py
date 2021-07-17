cdd74163bd4c5aa6377e1b1ec7b8cf129adc8dd3
gahvs <ga.vs_@outlook.com>
2021-07-17 11:44:06 -0300

    example - last feature

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from random import random
-from pylineartools import vector
-from pylineartools.vector import VectorComponent
-from pylineartools import Vector, VectorComponent
+from pylineartools import Vector
 
 v = Vector(1, 2, 3, [x for x in range(4, 9)], 9, 10)
 print(v.numbers())
